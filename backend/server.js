bc1c408b00127c2af5e7fdceba944eb4120170a9
Sellamuthu2007 <sellamuthur.cse2024@citchennai.net>
2025-12-20 00:08:19 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,14 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+ 
 
 const app = express();
 const PORT = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Innervex Backend is Live 🚀");
+});
 
 app.use(cors());
 app.use(bodyParser.json());
